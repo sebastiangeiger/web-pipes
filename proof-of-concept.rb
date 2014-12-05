d5d0d271c36c5cf4057f546d1ff4519accec95e1
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-12-05 08:07:25 -0500

    Retriving projects from pivotal works

--- proof-of-concept.rb
+++ proof-of-concept.rb
@@ -1,4 +1,32 @@
 require 'therubyracer'
+require 'faraday'
+require 'json'
+
+raise "Please set your PIVOTAL_TOKEN" unless ENV["PIVOTAL_TOKEN"]
+class PivotalTracker
+
+  def connection
+    @connection ||= Faraday.new(:url => 'https://www.pivotaltracker.com') do |faraday|
+      faraday.headers['X-TrackerToken'] = ENV["PIVOTAL_TOKEN"]
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
+  def projects
+    response = connection.get "/services/v5/projects"
+    raise unless response.success?
+    JSON.parse(response.body)
+  end
+
+end
+
+script = <<EOF
+  var project = pivotal.projects.filter(function(project){
+    return project.name === "ClariStream Management System";
+  })[0];
+  project.id;
+EOF
 
 cxt = V8::Context.new
-p cxt.eval('7 * 6')
+cxt['pivotal'] = PivotalTracker.new
+p cxt.eval(script)
