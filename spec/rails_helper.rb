fda5cab0578ee611e7022437da98e1723f854acb
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-12-17 07:28:18 -0500

    Moving RSpec configuration to spec_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'capybara/mechanize'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -48,18 +47,4 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-end
-
-Capybara.register_driver :mechanize do |app|
-  Capybara::Mechanize::Driver.new(app)
-end
-
-RSpec.configure do |c|
-  c.around do |example|
-    return unless example.metadata[:external]
-    old_driver = Capybara.current_driver
-    Capybara.current_driver = :mechanize
-    example.run
-    Capybara.current_driver = old_driver
-  end
 end
