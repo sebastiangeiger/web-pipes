bab7db490083fd5e222762e6179318b9e32ea859
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-12-25 17:01:36 -0500

    Showing a code field on the job page

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,16 @@ class Job < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
 
   has_many :code_versions
+
+  def code
+    if latest_code_version
+      latest_code_version.code
+    else
+      ''
+    end
+  end
+
+  def latest_code_version
+    code_versions.last
+  end
 end
---
+++ spec/features/add_code_to_job_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'support/sign_in_helpers'
+
+feature 'Code' do
+  include SignInHelpers
+
+  scenario 'add code to a job' do
+    fake_sign_in!
+    visit '/'
+    click_on 'New Job'
+    fill_in :job_name, with: 'Job #1'
+    click_on 'Next'
+    fill_in :job_code, with: 'console.log("Hello World");'
+  end
+end
