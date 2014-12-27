e1f732b09a850169efc644b808c896d75e2ceb3b
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-12-27 00:02:55 -0500

    Styling forms with errors and segment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,12 @@ module ApplicationHelper
     error_messages = model.errors.full_messages.map do |message|
       content_tag(:li, message)
     end
-    error_lis = error_messages.join.html_safe
-    content_tag(:ul, error_lis, class: 'errors') if error_messages.any?
+    if error_messages.any?
+      error_list = error_messages.join.html_safe
+      error_list = content_tag(:ul, error_list).html_safe
+      content_tag(:div, error_list, class: 'ui error message')
+    else
+      ''
+    end
   end
 end
