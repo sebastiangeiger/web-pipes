59275967c32ad5dd2c961bf76c6e3062d82df45c
Sebastian Geiger <sebastian.geiger@gmail.com>
2015-01-03 15:53:15 -0500

    Test for execution

--- spec/features/add_code_to_job_spec.rb
+++ spec/features/add_code_to_job_spec.rb
@@ -10,8 +10,9 @@ feature 'Code' do
     click_on 'New Job'
     fill_in :job_name, with: 'Job #1'
     click_on 'Next'
-    fill_in :job_code, with: 'console.log("Hello World");'
+    fill_in :job_code, with: 'console.log("1+2=" + (1+2));'
     click_on 'Save & Test'
-    expect(page).to have_content 'console.log("Hello World");'
+    expect(page).to have_content 'console.log("1+2=" + (1+2));'
+    expect(page).to have_content '1+2=3'
   end
 end
